482d7f63a15a499a09547d997530542841e08e0f
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-07-31 00:23:23 -0500

    create session controller

--- quickly/web/controllers/session_controller.ex
+++ quickly/web/controllers/session_controller.ex
@@ -5,4 +5,20 @@ defmodule Quickly.SessionController do
     render conn, "new.html"
   end
 
+  def create(conn, params = %{"email" => email, "password" => password}) do
+    nil
+  end
+  def crete(conn, %{"session" => session_params}) do
+    case Quickly.Session.login(session_params, Quickly.Repo) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user, user.id)
+        |> put_flash(:info, "Has iniciado sesión")
+        |> redirect(to: "/")
+      :error ->
+        conn
+        |> put_flash(:info, "Email o Password incorrecto")
+        |> render("new.html")
+    end
+  end
 end
